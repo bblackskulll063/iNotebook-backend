1461d398dbdfd62169059fb1c1816eff11c761b2
bblackskulll063 <98683404+bblackskulll063@users.noreply.github.com>
2023-09-13 22:47:27 +0530

    changes

--- db.js
+++ db.js
@@ -2,7 +2,7 @@ const mongoose = require('mongoose');
 require('dotenv').config();
 // const url = "mongodb://127.0.0.1:27017/iNotebook"
 const url = "mongodb+srv://himanshu412:Inotebook8130@cluster0.s0bd9hm.mongodb.net/?retryWrites=true&w=majority"
-console.log(process.env.MONGO_ATLAS_URL)
+// console.log(process.env.MONGO_ATLAS_URL)
 const connectToMongo = async ()=>{
     try {
         await mongoose.connect(url);
